7bf5a4fc5c5eb2d2a873feeb5be07fdae0fb5d55
Pablo Gramajo <pablo.gramajo@jbknowledge.com>
2020-01-03 15:13:53 -0300

    loading was centered

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -5,17 +5,15 @@ const Loading = () => (
     <div tabIndex="-1" style={{ position: 'relative', zIndex: 1050, display: 'block' }}>
         <div className="">
             <div className="modal fade show" role="dialog" tabIndex="-1" style={{ display: 'block' }}>
-                <div className="modal-dialog" role="document">
+                <div className="modal-dialog" style={{ top: '38%', left: '13%' }} role="document">
                     <div className="">
-                        <ReactLoading type={"spin"} color="black" height={467} width={175} />
+                        <ReactLoading type={"spin"} color="black" height={267} width={75} />
                     </div>
                 </div>
             </div>
             <div className="modal-backdrop fade show"></div>
         </div>
     </div>
-)
-
-
+);
 
 export default Loading
